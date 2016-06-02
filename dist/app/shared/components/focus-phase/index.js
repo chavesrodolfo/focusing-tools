44e69bd453a44abad85e3c0f4e388c9483739357
Cory Rylan <splintercode.cb@gmail.com>
2016-06-01 19:12:12 -0500

    Deps updated, removed dist files

--- dist/app/shared/components/focus-phase/index.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-var focus_phase_component_1 = require('./focus-phase.component');
-exports.FocusPhaseComponent = focus_phase_component_1.FocusPhaseComponent;
-//# sourceMappingURL=index.js.map
--- dist/app/shared/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-function __export(m) {
-    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
-}
-__export(require('./services/auth.service'));
-__export(require('./services/data.service'));
-__export(require('./services/notification.service'));
-__export(require('./services/timer.service'));
-//# sourceMappingURL=index.js.map
